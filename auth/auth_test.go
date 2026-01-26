9fa0791d2a35c081ea61a574d6488994e030e7c9
Dylan McCormick <dylanm.bsa@gmail.com>
2026-01-26 08:42:02 -0500

    comment test

--- auth/auth_test.go
+++ auth/auth_test.go
@@ -1,32 +1,32 @@
 package auth
 
-import (
-	"context"
-	"fmt"
-	"net/http/httptest"
-	"testing"
-	"time"
-)
-
-func TestPost(t *testing.T) {
-	sm := NewSessionManager()
-	sm.Run(context.TODO())
-
-	session := &Session{}
-	session.SessionId = "1234"
-	sendDeviceRequest(session)
-	t.Logf("%#v\n", session)
-	sm.AddSession(session)
-
-	ticker := time.NewTicker(10 * time.Second)
-
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
-
-	for range ticker.C {
-		if HandleAuthCheck(sm, session.SessionId, w, r) {
-			ticker.Stop()
-			fmt.Printf("%#v\n", session)
-		}
-	}
-}
+// import (
+// 	"context"
+// 	"fmt"
+// 	"net/http/httptest"
+// 	"testing"
+// 	"time"
+// )
+//
+// func TestPost(t *testing.T) {
+// 	sm := NewSessionManager()
+// 	sm.Run(context.TODO())
+//
+// 	session := &Session{}
+// 	session.SessionId = "1234"
+// 	sendDeviceRequest(session)
+// 	t.Logf("%#v\n", session)
+// 	sm.AddSession(session)
+//
+// 	ticker := time.NewTicker(10 * time.Second)
+//
+// 	w := httptest.NewRecorder()
+// 	r := httptest.NewRequest("GET", "/", nil)
+//
+// 	for range ticker.C {
+// 		if HandleAuthCheck(sm, session.SessionId, w, r) {
+// 			ticker.Stop()
+// 			fmt.Printf("%#v\n", session)
+// 		}
+// 	}
+// }
