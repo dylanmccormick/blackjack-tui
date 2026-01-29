d400d7a3ed31878f82cbf31aba6c685675ea0ee9
Dylan McCormick <dylanm.bsa@gmail.com>
2026-01-28 20:10:29 -0500

    Add goreleaser

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"os"
 
 	"github.com/dylanmccormick/blackjack-tui/client"
 	"github.com/dylanmccormick/blackjack-tui/server"
@@ -13,10 +12,6 @@ func main() {
 	function := flag.String("f", "tui", "use this flag to determine what function the app will serve. Available options: 'tui', 'server'")
 	mockFlg := flag.Bool("mock", false, "add this flag to run in MOCK mode. You will not be able to connect to a websocket")
 	flag.Parse()
-	args := os.Args
-	if len(args) < 2 {
-		os.Exit(1)
-	}
 	switch *function {
 	case "tui":
 		client.RunTui(mockFlg)
